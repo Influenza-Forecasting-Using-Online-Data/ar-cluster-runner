170c05d40876aa9a3e415f84acfbe176ec02275a
Radu <radu.echim.19@ucl.ac.uk>
2022-04-07 01:20:58 +0300

    Add number of steps argument to run.sh

--- run.sh
+++ run.sh
@@ -3,21 +3,20 @@
 remote_path=/disks/sdb/$user/ar-cluster-runner/ar_trainer_cluster_out
 
 usage () {
-  echo "Usage: run <trainer> | <autoarima>"
+  echo "Usage: run <trainer> <steps> | <autoarima> <steps>"
   echo ""
-  echo "trainer		run ar_cluster_trainer.py detached"
-  echo "autoarima	run ar_autoarima.py detached"
+  echo "trainer         run ar_cluster_trainer.py detached"
+  echo "autoarima       run ar_autoarima.py detached"
+  echo "step            num steps for multi-step prediction"
 }
 
 if [ -z $1 ]; then
    # No argument
    usage
 elif [ $1 = "trainer" ]; then
-   nohup python ar_trainer_cluster.py > trainer.out 2>&1 &
+   nohup python ar_trainer_cluster.py $2 > trainer.out 2>&1 &
 elif [ $1 = "autoarima" ]; then
    nohup python ar_autoarima.py > autoarima.out 2>&1 &
 else
    usage
 fi
-
-
